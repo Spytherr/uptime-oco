e32345a1917f20ff63093377ab23d96402ed7bd0
Spytherr <pajakmateusz2007@gmail.com>
2026-08-19 20:15:25 +0200

    Add Change Password  and fix login ui

--- uptime-oco/Controllers/AccountController.cs
+++ uptime-oco/Controllers/AccountController.cs
@@ -8,7 +8,9 @@ namespace uptime_oco;
 
 public class AccountController(
     SignInManager<ApplicationUser> signInManager,
-    IValidator<LoginViewModel> validator) : Controller
+    UserManager<ApplicationUser> userManager,
+    IValidator<LoginViewModel> loginValidator,
+    IValidator<ChangePasswordViewModel> changePasswordValidator) : Controller
 {
     [HttpGet]
     [AllowAnonymous]
@@ -26,7 +28,7 @@ public class AccountController(
     {
         ViewData["ReturnUrl"] = returnUrl;
 
-        var validationResult = await validator.ValidateAsync(model);
+        var validationResult = await loginValidator.ValidateAsync(model);
         if (!validationResult.IsValid)
         {
             foreach (var error in validationResult.Errors)
@@ -53,6 +55,51 @@ public class AccountController(
 
         ModelState.AddModelError(string.Empty, "Invalid login attempt.");
         return View(model);
+    }
+
+    [HttpGet]
+    [Authorize]
+    public IActionResult ChangePassword()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Authorize]
+    [ValidateAntiForgeryToken]
+    [EnableRateLimiting("login")]
+    public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+    {
+        var validationResult = await changePasswordValidator.ValidateAsync(model);
+        if (!validationResult.IsValid)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+            return View(model);
+        }
+
+        var user = await userManager.GetUserAsync(User);
+        if (user is null)
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+
+        var result = await userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(model);
+        }
+
+        await signInManager.RefreshSignInAsync(user);
+        TempData["StatusMessage"] = "Your password has been changed.";
+        return RedirectToAction(nameof(ChangePassword));
     }
 
     [HttpPost]
---
+++ uptime-oco/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,8 @@
+namespace uptime_oco;
+
+public class ChangePasswordViewModel
+{
+    public string CurrentPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+    public string ConfirmNewPassword { get; set; } = string.Empty;
+}
